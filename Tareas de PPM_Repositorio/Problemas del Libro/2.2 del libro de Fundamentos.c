d90d3131ad23668e3d6336b77bffcaad103b727b
EzequielPacheco <113851602+EzequielPacheco@users.noreply.github.com>
2022-10-17 17:05:54 -0600

    tareas y problemas del libro de fundamento
    aqui estan los problemas del libro de fundamento y dos tareas.

---
+++ Tareas de PPM_Repositorio/Problemas del Libro/2.2 del libro de Fundamentos.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main (void)
+{
+float PRE, NPR;
+printf("ingrese el precio del producto: ");
+scanf ("%f", &PRE);
+
+if (PRE > 1500);
+{
+    NPR =PRE * 1.11;
+printf ("\nNuevo precio : %7.2d", NPR );
+}
+}
+
+//NO SE PORQUE ESTE PROGRAMA NO ME FUNCION, LO COPIE TAL Y COMO ESTA EN EL LIBRO.
---
+++ Tareas de PPM_Repositorio/Problemas del Libro/Programa 3.4 del libro de fundamento.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+void main(void)
+{
+int NUM;
+long CUA, SUC = 0;
+printf("\nIngrese un número entero -0 para terminar-:\t");
+scanf("%d", &NUM);
+while (NUM)
+/* Observa que la condición es verdadera mientras el entero es diferente de cero. */
+{
+CUA = pow (NUM, 2);
+printf("%d al cubo es %ld", NUM, CUA);
+SUC = SUC + CUA;
+printf("\nIngrese un número entero -0 para terminar-:\t");
+scanf("%d", &NUM);
+}
+printf("\nLa suma de los cuadrados es %ld", SUC);
+}
